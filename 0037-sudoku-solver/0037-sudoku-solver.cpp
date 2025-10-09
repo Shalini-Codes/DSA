bb10bef05d8dbbe94de21e9a36cd69d846a356d0
Shalini Bhagat <shalini79801@gmail.com>
2025-10-09 21:45:36 +0530

    Time: 375 ms (40.45%), Space: 8.7 MB (65.12%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(vector<vector<char>>& board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            // Check row
+            if (board[row][i] == c) return false;
+            // Check column
+            if (board[i][col] == c) return false;
+            // Check 3x3 sub-box
+            int subRow = 3 * (row / 3) + i / 3;
+            int subCol = 3 * (col / 3) + i % 3;
+            if (board[subRow][subCol] == c) return false;
+        }
+        return true;
+    }
+
+    bool solve(vector<vector<char>>& board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(board, row, col, c)) {
+                            board[row][col] = c;
+                            if (solve(board)) return true;
+                            else board[row][col] = '.';  // backtrack
+                        }
+                    }
+                    return false; // if no valid number found, backtrack
+                }
+            }
+        }
+        return true; // puzzle solved
+    }
+
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+};
