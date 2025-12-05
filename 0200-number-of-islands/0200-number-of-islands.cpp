e1b84905c4555dd3f0c9355eff881b5c268c27d1
Shalini Bhagat <shalini79801@gmail.com>
2025-12-05 14:51:05 +0530

    Time: 30 ms (35.69%), Space: 16.4 MB (67.55%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void dfs(vector<vector<char>>& grid, int i, int j) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        // boundary or water check
+        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0')
+            return;
+
+        grid[i][j] = '0'; // mark visited
+
+        // explore 4 directions
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+    
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int count = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    count++;        // found a new island
+                    dfs(grid, i, j); // mark entire island as visited
+                }
+            }
+        }
+
+        return count;
+    }
+};
